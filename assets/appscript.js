ff30631e846817ac415989cdcc61ee10cea20072
Mustafa Farah <mustafa.farah@outlook.com>
2018-03-31 13:55:25 -0500

    added firebase to appscript

--- assets/appscript.js
+++ assets/appscript.js
@@ -1,3 +1,14 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCoBir9oSNY2AP_7FHwnxh3nTJDgEmLvA4",
+    authDomain: "project-startmyday.firebaseapp.com",
+    databaseURL: "https://project-startmyday.firebaseio.com",
+    projectId: "project-startmyday",
+    storageBucket: "",
+    messagingSenderId: "81322307189"
+  };
+  firebase.initializeApp(config);
+
 $(document).ready(function(){
 
     let returningUser = 0;
