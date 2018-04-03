cdb1170fb64a4977821d541c7c9873c185ec4427
Mustafa Farah <mustafa.farah@outlook.com>
2018-04-02 20:34:57 -0500

    adding localstorage and pulling it down

--- assets/appscript.js
+++ assets/appscript.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
   // Initialize Firebase
   var config = {
@@ -11,21 +10,21 @@ $(document).ready(function () {
   };
   firebase.initializeApp(config);
 
-  let database = firebase.database();
+  let db = firebase.database();
 
-  $("#submit").on("click", function(event) {
-  event.preventDefault();
+  $("#submit").on("click", function (event) {
+    event.preventDefault();
 
-  let returningUser = 0;
-  let userName = "mustafa";
-  let userAddress = "UoM Campus";
-  let event = ["#eventTitle", "#eventLocation", "#eventTime"];
-  let gifTerms = ["IDK"];
-  console.log("username works?");
+    let returningUser = 0;
+    let userName = ["mustafa"];
+    let userAddress = "UoM Campus";
+    let event = ["#eventTitle", "#eventLocation", "#eventTime"];
+    let gifTerms = ["IDK"];
+    console.log("username works?");
 
   });
-  
-  database.ref('users/' + userName).set({
+
+  db.ref('users/' + userName).set({
     userName: userName,
     userAddress: userAddress,
     returningUser: returningUser,
@@ -33,8 +32,23 @@ $(document).ready(function () {
     gifTerms: gifTerms
   });
 
+  function save(user) {
+    var isAlreadyInserted = false;
+    //loop through your username array to check
+    for (var i = 0; i < username.length; i++) {
+      if (userName[i] === currentUserName) {
+        isAlreadyInserted = true;
+      }
+    }
+    // Get the username array from localStorage if available, or create an empty array
+    if (username.indexOf(user.value) === -1) {
+      username.push(user.value);
+    }
 
-
+    // Convert the array into string and then store in the localStorage
+    localStorage.setItem('userName', userName.toString());
+  }
+  Console.log("saving stuff to local storage")
 
 
 
